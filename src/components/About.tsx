44e81ddb0de937b548c466568946c302427e7f7b
Mohammad Ali Haidar <mohammadali.hd3@gmail.com>
2026-08-14 14:28:24 +0200

    Added meta tag

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,7 @@ function About(){
     const about: AboutType = {drawnImg: undefined, realImg: "https://res.cloudinary.com/s7eflphu/image/upload/v1786555848/mewebsite_v3dj81.png", description: "I am a creative computer scientist. I love building new experiences for people and myself every day. I am also very passionate about nature, space and art."}
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 ml-auto mr-auto bg-[#261732] text-[#D6D0C5]">
+            <meta name="keywords" content="amby ambyintech React Developer JavaScript TypeScript Python Belgium HTML CSS TailwindCSS Tailwind Programming Programmer Software Engineering Engineer"></meta>
             <img className="w-full max-w-xs md:max-w-none md:w-1/2 md:ml-90" src={about.realImg} alt="portrait of Amby"></img>
             <div className="pt-8 pb-10 md:pt-30 md:pb-0">
             <h2 className="text-5xl sm:text-7xl md:text-9xl bentham-regular text-center md:text-left md:pl-20 px-6 md:px-0"><b>i'm amby</b></h2>
