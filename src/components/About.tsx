840fe560575521ed06355ff5d63d6b90dea64774
Mohammad Ali Haidar <mohammadali.hd3@gmail.com>
2026-08-14 14:23:47 +0200

    Added degrees

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,24 @@ function About(){
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 ml-auto mr-auto bg-[#261732] text-[#D6D0C5]">
             <img className="w-full max-w-xs md:max-w-none md:w-1/2 md:ml-90" src={about.realImg} alt="portrait of Amby"></img>
-            <div className="pt-8 pb-10 md:pt-50 md:pb-0">
+            <div className="pt-8 pb-10 md:pt-30 md:pb-0">
             <h2 className="text-5xl sm:text-7xl md:text-9xl bentham-regular text-center md:text-left md:pl-20 px-6 md:px-0"><b>i'm amby</b></h2>
             <div className="text-center md:text-right px-6 md:px-0 md:pr-100 text-xl md:text-3xl bentham-regular pt-6 md:pt-13 md:h-90">
                 <b>{about.description}</b>
+                <div className="flex flex-row mt-10 w-full mx-auto py-4 md:py-5 px-10 md:px-13 border-5 border-dashed border-[#261732] bg-[#f3f3f3] text-[#261732]">
+                    <img className="w-1/5 h-1/5" src={"https://res.cloudinary.com/s7eflphu/image/upload/v1786691355/logo_vub_wit_ncdgpe.png"} alt="Lebanese University Logo"></img>
+                    <div className="grid grid-cols-1">
+                        <p className="text-sm">Vrije Universiteit Brussel</p>
+                        <p className="text-2xl">Master of Applied Sciences and Engineering: Computer Science</p>
+                    </div>
+                </div>
+                <div className="flex flex-row mt-5 mx-auto py-4 md:py-5 px-10 md:px-13 border-5 border-dashed border-[#261732] bg-[#f3f3f3] text-[#261732]">
+                    <img className="w-1/5 pl-1 h-1/6 pt-1" src={"https://res.cloudinary.com/s7eflphu/image/upload/v1786691307/LULogo_pnm18t.png"} alt="Lebanese University Logo"></img>
+                    <div className="grid grid-cols-1">
+                        <p className="text-sm">Lebanese University</p>
+                        <p className="text-2xl">Bachelor of Science: Computer Science</p>
+                    </div>
+                </div>
             </div>
             </div>
         </div>
