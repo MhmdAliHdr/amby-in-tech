b03705294f72011e0162dfff8c63e8960440d19e
Mohammad Ali Haidar <mohammadali.hd3@gmail.com>
2026-08-15 10:55:32 +0200

    Modified mobile version

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ function About(){
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 ml-auto mr-auto bg-[#261732] text-[#D6D0C5]">
             <meta name="keywords" content="amby ambyintech React Developer JavaScript TypeScript Python Belgium HTML CSS TailwindCSS Tailwind Programming Programmer Software Engineering Engineer"></meta>
-            <img className="w-full mx-auto rounded-full aspect-square object-cover md:aspect-auto md:rounded-none max-w-xs md:max-w-none md:w-1/2 md:ml-90" src={about.realImg} alt="portrait of Amby"></img>
+            <img className="w-full mx-auto rounded-full aspect-square object-cover object-top md:aspect-auto md:rounded-none max-w-xs md:max-w-none md:w-1/2 md:ml-90" src={about.realImg} alt="portrait of Amby"></img>
             <div className="pt-8 pb-10 md:pt-30 md:pb-0">
             <h2 className="text-5xl sm:text-7xl md:text-9xl bentham-regular text-center md:text-left md:pl-20 px-6 md:px-0"><b>i'm amby</b></h2>
             <div className="text-center md:text-right px-6 md:px-0 md:pr-100 text-xl md:text-3xl bentham-regular pt-6 md:pt-13 md:h-90">
